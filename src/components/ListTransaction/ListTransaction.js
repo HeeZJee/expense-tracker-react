c9bf1aa866ccf72bb70e461db7da7e0a5f1f2b51
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-06-20 01:16:36 +0500

    updated UI

--- src/components/ListTransaction/ListTransaction.js
+++ src/components/ListTransaction/ListTransaction.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import './ListTransaction'
+import './ListTransaction.css'
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+
 
 const ListTransaction = () => {
     return (
         <div className="list">
             <h3>History</h3>
+            <ul id="list" class="list">
+                <li class="minus">
+                    Cash <span>-$400</span><button class="delete-btn"><DeleteOutlinedIcon /></button>
+                </li>
+
+                <li class="minus">
+                    Cash <span>-$400</span><button class="delete-btn"><DeleteOutlinedIcon /></button>
+                </li>
+                <li class="minus">
+                    Cash <span>-$400</span><button class="delete-btn"><DeleteOutlinedIcon /></button>
+                </li>
+                <li class="minus">
+                    Cash <span>-$400</span><button class="delete-btn"><DeleteOutlinedIcon /></button>
+                </li>
+                <li class="minus">
+                    Cash <span>-$400</span><button class="delete-btn"><DeleteOutlinedIcon /></button>
+                </li>
+            </ul>
         </div>
     )
 }

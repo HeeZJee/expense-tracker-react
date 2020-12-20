57bd67b2edfc65e088bd4496ccdcc8d466f7b533
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-12-21 03:38:10 +0500

    uncomment fetch

--- public/sw.js
+++ public/sw.js
@@ -12,11 +12,6 @@ const cachePath = [
   "/index.html",
   "https://fonts.gstatic.com/s/lato/v17/S6uyw4BMUTPHjx4wXg.woff2",
   "https://fonts.googleapis.com/css?family=Lato&display=swap",
-  "/static/js/bundle.js",
-  "/static/js/0.chunk.js",
-  "/static/js/1.chunk.js",
-  "/static/js/main.chunk.js",
-  "main.43318790f93790fc0464.hot-update.js",
 ];
 
 self.addEventListener("register", () => console.log("registered sw.js"));
@@ -31,11 +26,11 @@ self.addEventListener("install", (e) => {
   );
 });
 
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(
-//     caches
-//       .match(e.request)
-//       .then((cacheRes) => cacheRes || fetch(e.request))
-//       .catch((e) => console.log("Service Worker Fetch Error", e))
-//   );
-// });
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((cacheRes) => cacheRes || fetch(e.request))
+      .catch((e) => console.log("Service Worker Fetch Error", e))
+  );
+});
